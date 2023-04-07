9bac9d74e3726400a8a2b45dad3b5078c59697ba
AbdulWasay0608 <abdulwasay0608@gmail.com>
2023-04-08 00:53:04 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+// var a = prompt( "Enter Total Value:" )
+// var b = prompt( "Enter Percentage:" )
+
+// var c = a * b / 100;
+
+
+//  alert( c + "%" )
+
+
+
+
+
+var weight = 150;
+var time = 5;
+
+
+     if (weight > 300 && time < 6) {
+     alert("Come to our tryout!");
+     }
+     else {
+     alert("Come to our cookout!");
+     }
+   
+     if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+     alert("Come to our tryout!");
+     }
+     else {
+     alert("Come to our cookout!");
+     }
+    
+     if (SAT > avg || GPA > 2.5 || sport === "football") {
+     alert("Welcome to Bubba State!");
+     }
+     else {
+     alert("Have you looked into appliance repair?");
+     }
+    if (age > 65 || age < 21 && res === "U.S.");
+
+
+
+    if ((x === y || a === b) && c === d) {
+         g = h;
+         }
+         else {
+         e = f;
+         }
+        
+
